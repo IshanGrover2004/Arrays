c0e2d5d70d0f5dab9d29b39e568ea539cbe77e41
Ishan Grover <123376573+IshanGrover2004@users.noreply.github.com>
2023-04-04 12:52:50 +0530

    Add files via upload

---
+++ running_sum_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    int arr[]={1,2,3,4};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    vector<int> ans;
+    int sum=0;
+    for(int i:arr){
+        sum+=i;
+        ans.push_back(sum);
+    }
+    for(int j:ans){
+        cout<<j<<" ";
+    }
+}
